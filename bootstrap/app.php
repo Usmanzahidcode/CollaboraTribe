e35b5c04ecbc6d9cd05a48b41998bc3b24236c83
Usman Zahid <developerusman@yahoo.com>
2024-11-27 20:30:32 +0500

    Route cleaning

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,28 @@
 <?php
 
+use App\Http\Middleware\IsActive;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\IsLoggedIn;
+use App\Http\Middleware\IsVerified;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'authenticated' => IsLoggedIn::class,
+            'unauthenticated' => IsLoggedIn::class,
+            '',
+            'admin' => IsAdmin::class,
+            'active' => IsActive::class,
+            'verified' => IsVerified::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\NotLoggedIn;
-use App\Http\Middleware\IsAdmin;
-use App\Http\Middleware\IsLoggedIn;
-use App\Http\Middleware\IsLoggedInAndActive;
 use Illuminate\Support\Facades\Route;
 
 
@@ -14,16 +11,34 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
+Route::group(['middleware' => ['unauthenticated']], function () {
+    Route::post('/users/authenticate', [UserController::class, 'handleLogin'])->name('users.signin_submit');
+    Route::get('/user/sign-in', [UserController::class, 'signIn'])->name('users.signin');
 
-Route::get('/user/sign-out', [UserController::class, 'signOut'])->name('users.signout')->middleware(IsLoggedIn::class);
-Route::post('/users/authenticate', [UserController::class, 'handleLogin'])->name('users.signin_submit')->middleware(NotLoggedIn::class);
-Route::get('/user/sign-in', [UserController::class, 'signIn'])->name('users.signin')->middleware(NotLoggedIn::class);
+    // Password
+    Route::get('/reset-password', function () {
+        return view('auth.password_reset_form');
+    })->name('reset.password');
+
+    Route::post('/reset-password-email-submit', [UserController::class, 'passwordReset'])
+        ->name('reset.submit');
+
+    Route::get('/update-password/{token}', [UserController::class, 'passwordResetForm'])
+        ->name('reset.form');
+
+    Route::post('/update-password-submit/{email}', [UserController::class, 'passwordResetUpdate'])
+        ->name('reset.submitted');
+});
 
 
-Route::middleware(IsLoggedInAndActive::class)->group(function () {
+Route::group(['middleware' => ['authenticated', 'active', 'verified']], function () {
+
+    Route::get('/user/sign-out', [UserController::class, 'signOut'])->name('users.signout');
+
     // User Routes
     Route::resource('users', UserController::class);
     Route::post('/change/password', [UserController::class, 'changePassword'])->name('user.changepassword');
+    Route::get('/account/manage/', [UserController::class, 'manageAccount'])->name('user.manage');
 
     // Project Routes
     Route::resource('projects', ProjectController::class);
@@ -38,42 +53,20 @@ Route::middleware(IsLoggedInAndActive::class)->group(function () {
     //Project status updates
     Route::post('/project/change-status/{project}', [ProjectController::class, 'changeStatus'])->name('project.changestatus');
 
-    Route::middleware(IsAdmin::class)->group(function () {
+    Route::group(['middleware' => ['admin']], function () {
         Route::get('users', [UserController::class, 'index']);
         Route::post('/project/admin-approval/{project}', [ProjectController::class, 'changeStatusByAdmin'])->name('project.adminapproval');
         Route::get('admin/projects/manage', [ProjectController::class, 'manageProjects'])->name('projects.manage');
         Route::post('admin/users/{user}/ban', [UserController::class, 'adminBan'])->name('users.ban');
     });
-
-
 });
 
 Route::resource('users', UserController::class)->only(['create', 'store'])->middleware(NotLoggedIn::class);
 Route::resource('posts', CommentController::class)->except(['index', 'show', 'create']);
 
-//Manage Account
-Route::get('/account/manage/', [UserController::class, 'manageAccount'])->name('user.manage')->middleware(IsLoggedIn::class);
-
 Route::get('/verify-email/{token}', [UserController::class, 'verifyEmail'])->name('verify.email');
 Route::get('/regenerate/email-token/{user}', [UserController::class, 'regenerateEmailToken'])->name('regenerate.emailtoken');
 
-
-// TODO: This shit also needs proper check and cleanup
-Route::get('/reset-password', function () {
-    return view('auth.password_reset_form');
-})->middleware(NotLoggedIn::class)->name('reset.password');
-
-Route::post('/reset-password-email-submit', [UserController::class, 'passwordReset'])
-    ->middleware(NotLoggedIn::class)
-    ->name('reset.submit');
-
-Route::get('/update-password/{token}', [UserController::class, 'passwordResetForm'])
-    ->middleware(NotLoggedIn::class)
-    ->name('reset.form');
-
-Route::post('/update-password-submit/{email}', [UserController::class, 'passwordResetUpdate'])
-    ->middleware(NotLoggedIn::class)
-    ->name('reset.submitted');
 
 Route::get('/404', function () {
     return view('fallback');
